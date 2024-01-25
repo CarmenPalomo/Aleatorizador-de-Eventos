abe5eba38ab31fc7abcacb747d6e93cb53aca95b
CarmenPalomo <carpal162@alu.centroafuera.es>
2024-01-25 22:18:06 +0100

    mercader modificaciones

--- app/src/main/java/com/example/personajecreacion/Mercader.kt
+++ app/src/main/java/com/example/personajecreacion/Mercader.kt
@@ -14,25 +14,26 @@ class Mercader : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mercader)
         val comerciar: Button = findViewById(R.id.comerciar)
-        val continuar : Button = findViewById(R.id.continuarM)
-        var imagen : ImageView = findViewById(R.id.mercaderF)
-        val comprar : Button = findViewById(R.id.comprar)
-        val vender : Button = findViewById(R.id.vender)
-        val cancelar : Button = findViewById(R.id.cancelar)
+        val continuar: Button = findViewById(R.id.continuarM)
+        var imagen: ImageView = findViewById(R.id.mercaderF)
+        val comprar: Button = findViewById(R.id.comprar)
+        val vender: Button = findViewById(R.id.vender)
+        val cancelar: Button = findViewById(R.id.cancelar)
+        val mochila: Mochila? = null
 
-        var art1 : TextView = findViewById(R.id.art1)
-        var art2 : TextView = findViewById(R.id.art2)
-        var art7 : TextView = findViewById(R.id.art7)
-        var art8 : TextView = findViewById(R.id.art8)
-        var art9 : TextView = findViewById(R.id.art9)
-        var art10 : TextView = findViewById(R.id.art10)
+        var art1: TextView = findViewById(R.id.art1)
+        var art2: TextView = findViewById(R.id.art2)
+        var art7: TextView = findViewById(R.id.art7)
+        var art8: TextView = findViewById(R.id.art8)
+        var art9: TextView = findViewById(R.id.art9)
+        var art10: TextView = findViewById(R.id.art10)
 
-        var cant1 : EditText = findViewById(R.id.cant1)
-        var cant2 : EditText = findViewById(R.id.cant2)
-        var cant7 : EditText = findViewById(R.id.cant7)
-        var cant8 : EditText = findViewById(R.id.cant8)
-        var cant9 : EditText = findViewById(R.id.cant9)
-        var cant10 : EditText = findViewById(R.id.cant10)
+        var cant1: EditText = findViewById(R.id.cant1)
+        var cant2: EditText = findViewById(R.id.cant2)
+        var cant7: EditText = findViewById(R.id.cant7)
+        var cant8: EditText = findViewById(R.id.cant8)
+        var cant9: EditText = findViewById(R.id.cant9)
+        var cant10: EditText = findViewById(R.id.cant10)
 
         val dbHelper2 = MercaderDataBase(this)
 
@@ -47,57 +48,145 @@ class Mercader : AppCompatActivity() {
                 val articulos = dbHelper2.getArticulos()
                 var num = 1
                 articulos.forEach {
-                    when(num){
-                        1->{
+                    when (num) {
+                        1 -> {
                             art1.setVisibility(View.VISIBLE)
                             cant1.setVisibility(View.VISIBLE)
                             art1.text = it.getNombre()
                         }
-                        2->{
+
+                        2 -> {
                             art2.setVisibility(View.VISIBLE)
                             cant2.setVisibility(View.VISIBLE)
                             art2.text = it.getNombre()
                         }
-                        7->{
+
+                        7 -> {
                             art7.setVisibility(View.VISIBLE)
                             cant7.setVisibility(View.VISIBLE)
                             art7.text = it.getNombre()
                         }
-                        8->{
+
+                        8 -> {
                             art8.setVisibility(View.VISIBLE)
                             cant8.setVisibility(View.VISIBLE)
                             art8.text = it.getNombre()
                         }
-                        9->{
+
+                        9 -> {
                             art9.setVisibility(View.VISIBLE)
                             cant9.setVisibility(View.VISIBLE)
                             art9.text = it.getNombre()
                         }
-                        10->{
+
+                        10 -> {
                             art10.setVisibility(View.VISIBLE)
                             cant10.setVisibility(View.VISIBLE)
                             art10.text = it.getNombre()
                         }
                     }
-                    num++
-                }
+                    num = 1
+                    articulos.forEach {
+                        when (num) {
+                            1 -> {
+                                if (cant1.text.toString().toInt() > 0) {
+                                    val articulo: Articulo = (it.copy(
+                                        nombre = it.getNombre(),
+                                        tipo = it.getTipo(),
+                                        peso = it.getPeso(),
+                                        unidades = cant1.text.toString().toInt(),
+                                        precio = 0
+                                    ))
+                                    mochila?.guardarArticulo(articulo)
+                                }
+                            }
 
+                            2 -> {
+                                if (cant2.text.toString().toInt() > 0) {
+                                    val articulo: Articulo = (it.copy(
+                                        nombre = it.getNombre(),
+                                        tipo = it.getTipo(),
+                                        peso = it.getPeso(),
+                                        unidades = cant2.text.toString().toInt(),
+                                        precio = 0
+                                    ))
+                                    mochila?.guardarArticulo(articulo)
+                                }
+                            }
+
+                            7 -> {
+                                if (cant7.text.toString().toInt() > 0) {
+                                    val articulo: Articulo = (it.copy(
+                                        nombre = it.getNombre(),
+                                        tipo = it.getTipo(),
+                                        peso = it.getPeso(),
+                                        unidades = cant7.text.toString().toInt(),
+                                        precio = 0
+                                    ))
+                                    mochila?.guardarArticulo(articulo)
+                                }
+                            }
+
+                            8 -> {
+                                if (cant8.text.toString().toInt() > 0) {
+                                    val articulo: Articulo = (it.copy(
+                                        nombre = it.getNombre(),
+                                        tipo = it.getTipo(),
+                                        peso = it.getPeso(),
+                                        unidades = cant8.text.toString().toInt(),
+                                        precio = 0
+                                    ))
+                                    mochila?.guardarArticulo(articulo)
+                                }
+                            }
+
+                            9 -> {
+                                if (cant9.text.toString().toInt() > 0) {
+                                    val articulo: Articulo = (it.copy(
+                                        nombre = it.getNombre(),
+                                        tipo = it.getTipo(),
+                                        peso = it.getPeso(),
+                                        unidades = cant9.text.toString().toInt(),
+                                        precio = 0
+                                    ))
+                                    mochila?.guardarArticulo(articulo)
+                                }
+                            }
+
+                            10 -> {
+                                if (cant10.text.toString().toInt() > 0) {
+                                    val articulo: Articulo = (it.copy(
+                                        nombre = it.getNombre(),
+                                        tipo = it.getTipo(),
+                                        peso = it.getPeso(),
+                                        unidades = cant10.text.toString().toInt(),
+                                        precio = 0
+                                    ))
+                                    mochila?.guardarArticulo(articulo)
+                                }
+                            }
+                        }
+                        num++
+                    }
+
+
+                }
+                vender.setOnClickListener {
+                    imagen.setImageResource(R.drawable.mochila)
+                }
+                cancelar.setOnClickListener {
+                    comerciar.setVisibility(View.VISIBLE)
+                    continuar.setVisibility(View.VISIBLE)
+                    comprar.setVisibility(View.INVISIBLE)
+                    vender.setVisibility(View.INVISIBLE)
+                    cancelar.setVisibility(View.INVISIBLE)
+                    imagen.setImageResource(R.drawable.mercader)
+                }
             }
-            vender.setOnClickListener {
-                imagen.setImageResource(R.drawable.mochila)
+            continuar.setOnClickListener {
+                val intent = Intent(this, Aventura::class.java)
+                startActivity(intent)
             }
-            cancelar.setOnClickListener {
-                comerciar.setVisibility(View.VISIBLE)
-                continuar.setVisibility(View.VISIBLE)
-                comprar.setVisibility(View.INVISIBLE)
-                vender.setVisibility(View.INVISIBLE)
-                cancelar.setVisibility(View.INVISIBLE)
-                imagen.setImageResource(R.drawable.mercader)
-            }
-        }
-        continuar.setOnClickListener {
-            val intent = Intent(this,Aventura::class.java)
-            startActivity(intent)
         }
     }
 }
