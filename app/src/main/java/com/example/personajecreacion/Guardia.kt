af1b308841d55a304bd24e01993b9c62e5a8587f
PabloRB24 <pabrod138@alu.centoafuera.es>
2024-02-13 10:49:52 +0100

    guardian

---
+++ app/src/main/java/com/example/personajecreacion/Guardia.kt
@@ -0,0 +1,11 @@
+package com.example.personajecreacion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Guardia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guardia)
+    }
+}
