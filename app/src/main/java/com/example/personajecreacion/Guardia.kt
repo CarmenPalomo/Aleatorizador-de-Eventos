709a97413c808deb4cc021c725d696a0bd54aa95
PabloRB24 <pabrod138@alu.centoafuera.es>
2024-02-13 11:01:36 +0100

    guardian

--- app/src/main/java/com/example/personajecreacion/Guardia.kt
+++ app/src/main/java/com/example/personajecreacion/Guardia.kt
@@ -1,11 +1,22 @@
 package com.example.personajecreacion
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Guardia : AppCompatActivity() {
+    private lateinit var boton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guardia)
+
+        boton = findViewById(R.id.botonEchado)
+
+        boton.setOnClickListener {
+            val intent = Intent(this, AventuraActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
