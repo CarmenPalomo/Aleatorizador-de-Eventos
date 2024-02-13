d7919fa3a520ca5ddcb7a1d2b1dbb54702f93723
PabloRB24 <pabrod138@alu.centoafuera.es>
2024-02-13 12:21:42 +0100

    Pelea

--- app/src/main/java/com/example/personajecreacion/HerreroActivity.kt
+++ app/src/main/java/com/example/personajecreacion/HerreroActivity.kt
@@ -1,11 +1,42 @@
 package com.example.personajecreacion
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HerreroActivity : AppCompatActivity() {
+    private lateinit var botonPelea : Button
+    private lateinit var botonMercader : Button
+    private lateinit var botonReparar : Button
+    private lateinit var botonCrear : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_herrero)
+
+        botonCrear = findViewById(R.id.crear)
+        botonReparar = findViewById(R.id.reparar)
+        botonPelea = findViewById(R.id.pelea)
+        botonMercader = findViewById(R.id.mercader)
+
+        botonCrear.setOnClickListener {
+
+        }
+
+        botonReparar.setOnClickListener {
+
+        }
+
+        botonPelea.setOnClickListener {
+
+        }
+
+        botonMercader.setOnClickListener {
+            var intent : Intent = Intent(this,AventuraActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/personajecreacion/Pelea.kt
@@ -0,0 +1,11 @@
+package com.example.personajecreacion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pelea : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pelea)
+    }
+}
